934881e2df619cab7bd7122a8792bb919ee7bfa2
f1rze <47173672+f1rze@users.noreply.github.com>
2019-03-08 12:25:16 +0300

    Update ParsingException.java

--- AllExceptions/ParsingException.java
+++ AllExceptions/ParsingException.java
@@ -4,14 +4,4 @@ public class ParsingException extends Exception {
     public ParsingException(String message,int position) {
         super(message+(position+1));
     }
-
-   /* static protected String positionShow(int index) {
-        StringBuilder pointer = new StringBuilder();
-        for (int i = 0; i < index; i++) {
-            pointer.append(' ');
-        }
-        pointer.append('|');
-        return pointer.toString();
-    }*/
-
 }
